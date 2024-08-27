1dbc453ccdf8b67777864cbf0655cdf47989b4c8
codeMountainEs <codemountain.es@icloud.com>
2024-08-27 12:14:08 +0200

    error Policy

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -20,7 +20,8 @@ class UserPolicy
      */
     public function view(User $user, User $model): bool
     {
-        //
+        return $user->isAdmin();
+
     }
 
     /**
@@ -28,7 +29,8 @@ class UserPolicy
      */
     public function create(User $user): bool
     {
-        //
+        return $user->isAdmin();
+
     }
 
     /**
@@ -36,7 +38,8 @@ class UserPolicy
      */
     public function update(User $user, User $model): bool
     {
-        //
+        return $user->isAdmin();
+
     }
 
     /**
@@ -44,7 +47,8 @@ class UserPolicy
      */
     public function delete(User $user, User $model): bool
     {
-        //
+        return $user->isAdmin();
+
     }
 
     /**
@@ -52,7 +56,8 @@ class UserPolicy
      */
     public function restore(User $user, User $model): bool
     {
-        //
+        return $user->isAdmin();
+
     }
 
     /**
@@ -60,6 +65,7 @@ class UserPolicy
      */
     public function forceDelete(User $user, User $model): bool
     {
-        //
+        return $user->isAdmin();
+
     }
 }
